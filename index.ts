31a1eec084b8833e33cd3f37194d6ea799ff5e0c
Miguel <miguel.viloria.247@gmail.com>
2023-08-02 20:55:12 -0400

    socket en index

--- index.ts
+++ index.ts
@@ -1,14 +1,28 @@
 import { connection, models } from "./src/db";
-import app from "./src/app";
 import { seedUsers } from "./src/env/seeds";
+import app from "./src/app";
+import http from 'http';
+import { Server } from 'socket.io';
 
 const { User } = models;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Socket its alive!!!!!')
+   io.emit('firstEvent', 'hello this is test')
+});
 
 connection
   .sync({ force: false })
   .then(() => {
     console.log("database connected");
-    app.listen(3001, async () => {
+    server.listen(3001, async () => {
       console.log("App is listening on port 3001!");
 
       const users = await User.findAll();
